0b4dbe02b349b1f711827c35a1d09e92c908ba3c
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-06-23 12:20:12 +0530

    update version

--- README.md
+++
@@ -1,2 +0,0 @@
-# ab-slide-ease
-A seamless and easy-to-use React slider library for smooth, flexible web interfaces.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import AbSlideElse from './AbSlideElse';
-import './AbSlideElse.css';
 
-const slides = [
+
+const autoImg = [
   { image: 'https://wallpapercave.com/wp/wp4659392.jpg', caption: 'Slide 1' },
   { image: 'https://images.pexels.com/photos/868483/pexels-photo-868483.jpeg?cs=srgb&dl=active-aerobics-balance-868483.jpg&fm=jpg', caption: 'Slide 2' },
   { image: 'https://www.lakeshoresf.com/wp-content/uploads/2021/04/Kids-Fitness-Training-Lincoln-Park-Chicago-scaled.jpeg', caption: 'Slide 3' },
@@ -13,7 +13,8 @@ const App = () => {
   return (
     <div>
       
-      <AbSlideElse slides={slides} interval={3000} />
+      <AbSlideElse slides={autoImg} interval={3000} />
+      <h1>hello good</h1>
     </div>
   );
 };
